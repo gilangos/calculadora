f46b8019cbc7a40d1a929cc729994cd5bc876f3c
gilangos <gil.goc@outlook.com>
2022-12-17 02:35:41 -0300

    feat: add new functions

--- calc.js
+++ calc.js
@@ -1,29 +1,58 @@
 var space = document.querySelector('.space');
 
-function insert(num){
-    let temcoisa = space.innerHTML;
-    space.innerHTML = temcoisa + num;
+
+function OperIsRepeated(arg){
+    let number = space.innerHTML;
+    if(arg === '*' || arg === '/'){
+        if(number[number.length - 1] === '*' || number[number.length - 1] === '/'){
+            return true
+        }
+    }
+    if(arg === '-' || arg === '+'){
+        if(number[number.length - 1] === '-' || number[number.length - 1] === '+'){
+            return true
+        }
+    }
+    return false
 }
+
+function IsEmpty(){
+    let hascontent = space.innerHTML ? true : false;
+    return hascontent;
+}
+
+function insert(num){
+    let number = space.innerHTML;
+
+    if(OperIsRepeated(num)) return
+    if(num === '*' || num === '/'){
+        if(IsEmpty){
+            return
+        }
+    }
+    // #### inserir ###
+    space.innerHTML = number + num;
+}
+
 
 function exclude(){
-
-    let temcoisa = space.innerHTML;
+    let number = space.innerHTML;
     // space.innerHTML = temcoisa.substring(0, temcoisa.length - 1);
-    space.innerHTML = temcoisa.slice(0,temcoisa.length - 1)
+    space.innerHTML = number.slice(0,number.length - 1)
 }
 
-function limpar(){
+
+function clean(){
     space.innerHTML = "";
 }
 
-function calcular(){
-    let temcoisa = space.innerHTML;
-    if(temcoisa){
-        space.innerHTML = eval(temcoisa);
+function calc(){
+    let number = space.innerHTML;
+    if(number){
+        space.innerHTML = eval(number);
     }
     else{
         space.innerHTML = 'empty'
-
         setTimeout(()=>{
             space.innerHTML = ""
         },2000)
